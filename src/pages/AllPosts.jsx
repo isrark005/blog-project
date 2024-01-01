c170a428ce8318106c9cf627fabd96a8f4e7c6cb
Israr Khan <isrark005@gmail.com>
2024-01-01 18:10:43 +0530

    fixed layout on all post page

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -19,11 +19,13 @@ export function AllPosts() {
     return (
         <div className='py-8'>
             <Container>
+            <div className='flex flex-wrap'>
                 {posts && posts.map((post)=> (
                    <div key={post.$id} className='p-2 w-1/4'>
                    <PostCard {...post} />
                </div>
                 ))}
+                </div>
             </Container>
         </div>
     )
